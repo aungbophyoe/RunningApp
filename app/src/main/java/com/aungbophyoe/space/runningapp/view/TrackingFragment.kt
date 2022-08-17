e9a3bf3bb44c8f693bdd0d63b3ae3eed5f9540ec
aungbophyoe <abbp1001@gmail.com>
2022-08-17 11:47:00 +0630

    setup proj

--- app/src/main/java/com/aungbophyoe/space/runningapp/view/TrackingFragment.kt
+++ app/src/main/java/com/aungbophyoe/space/runningapp/view/TrackingFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.aungbophyoe.space.runningapp.databinding.FragmentTrackingBinding
+import com.google.android.gms.maps.GoogleMap
 
 class TrackingFragment : Fragment(){
     private var _binding : FragmentTrackingBinding? = null
     private val binding get() = _binding
+    private var googleMap : GoogleMap? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,13 +23,49 @@ class TrackingFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.apply {
-
+        binding?.apply {
+            mapView.onCreate(savedInstanceState)
+            mapView.getMapAsync {
+                googleMap = it
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        binding?.mapView?.onDestroy()
         _binding = null
     }
+
+    override fun onResume() {
+        super.onResume()
+        binding?.mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding?.mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding?.mapView?.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding?.mapView?.onResume()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding?.mapView?.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding?.mapView?.onSaveInstanceState(outState)
+    }
+
+
 }
